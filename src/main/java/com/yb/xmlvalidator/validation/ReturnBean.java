f524263a5bcf4d69235e1a36eb07d1e1b2bcb787
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-15 18:00:31 -0400

    Update ReturnBean.java

--- src/main/java/com/yb/xmlvalidator/validation/ReturnBean.java
+++ src/main/java/com/yb/xmlvalidator/validation/ReturnBean.java
@@ -2,4 +2,29 @@ package com.yb.xmlvalidator.validation;
 
 public class ReturnBean {
 
+	private String msg;
+	private Object data;
+	
+	public ReturnBean(String message,Object data) {
+		this.msg = message;
+		this.data = data;
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+	public String toString() {
+		return this.msg+" "+this.data;
+	}
+	
 }
