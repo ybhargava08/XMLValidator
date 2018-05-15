48105bc3aceb6d9d8feabce25d5a9b7190699f2e
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-15 17:58:19 -0400

    Update UIController.java

--- src/main/java/com/yb/xmlvalidator/controller/UIController.java
+++ src/main/java/com/yb/xmlvalidator/controller/UIController.java
@@ -1,5 +1,13 @@
 package com.yb.xmlvalidator.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UIController {
 
+	@GetMapping("/xmlvalidator")
+	public String getLoadingPage() {
+		return "XmlValidate";
+	}
 }
