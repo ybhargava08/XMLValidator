d7301ed7be6bbb9a7dfabfcf4c65c3f23b3d92c5
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-15 17:59:41 -0400

    Update RegexConstants.java

--- src/main/java/com/yb/xmlvalidator/service/RegexConstants.java
+++ src/main/java/com/yb/xmlvalidator/service/RegexConstants.java
@@ -2,4 +2,9 @@ package com.yb.xmlvalidator.service;
 
 public class RegexConstants {
 
+	public static final String regexSchemalocation = ".+?(?i)(location)[\\s]*=[\\s]*\"(?i)(http)(.+)(\\.xsd|wsdl)\".*?";
+	public static final String regexSchemalocationFromFile = ".+?(?i)(location)[\\s]*=[\\s]*\"(.+?)(\\.xsd|wsdl)\".*?";
+	public static final String soapNS="http://schemas.xmlsoap.org/soap/envelope/";
+	public static final String removeCommentsRegex ="(?s)<!--.*?-->";
+	public static final String replaceNewLineRegex = "[\\t\\n\\r]+";
 }
