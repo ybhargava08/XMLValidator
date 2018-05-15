a70dd36fcb836a30d4a79dc18c248d4a3114ffa2
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-15 17:57:50 -0400

    Update ValidatorViewResolver.java

--- src/main/java/com/yb/xmlvalidator/UIXmlValidator/ValidatorViewResolver.java
+++ src/main/java/com/yb/xmlvalidator/UIXmlValidator/ValidatorViewResolver.java
@@ -1,5 +1,13 @@
 package com.yb.xmlvalidator.UIXmlValidator;
 
-public class ValidatorViewResolver {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class ValidatorViewResolver implements WebMvcConfigurer{
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/resources/static/");
+	}
 }
