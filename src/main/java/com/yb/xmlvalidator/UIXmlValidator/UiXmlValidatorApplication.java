09f0ff077c6e4d79ada87e1bbb2f87fa3a190746
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-16 16:11:04 -0400

    Update UiXmlValidatorApplication.java

--- src/main/java/com/yb/xmlvalidator/UIXmlValidator/UiXmlValidatorApplication.java
+++ src/main/java/com/yb/xmlvalidator/UIXmlValidator/UiXmlValidatorApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import com.yb.xmlvalidator.validation.XMLValidationCore;
 
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.yb.xmlvalidator"})
+@EnableAspectJAutoProxy
 public class UiXmlValidatorApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UiXmlValidatorApplication.class, args);
 	}
