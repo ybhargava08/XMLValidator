e9815cc9e273566d9d55e6b53e969c45e158221e
ybhargava08 <32349135+ybhargava08@users.noreply.github.com>
2018-05-16 16:12:32 -0400

    Update XmlValidatorController.java

--- src/main/java/com/yb/xmlvalidator/controller/XmlValidatorController.java
+++ src/main/java/com/yb/xmlvalidator/controller/XmlValidatorController.java
@@ -1,8 +1,11 @@
 package com.yb.xmlvalidator.controller;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,51 +18,52 @@ import org.xml.sax.SAXException;
 
 import com.yb.xmlvalidator.service.XmlValidatorService;
 import com.yb.xmlvalidator.validation.ReturnBean;
+import com.yb.xmlvalidator.validation.XMLValidationCore;
 
 @RestController
 public class XmlValidatorController {
-
+	
 	@Autowired
 	XmlValidatorService service;
 	
+	@Autowired
+	XMLValidationCore core;
+	
 	@PostMapping(value="/uploadfiles",consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ReturnBean getMultipleFiles(@RequestParam("schemafiles") MultipartFile[] schemaFiles,@RequestParam("xmlfile") MultipartFile xmlFile) {
+	public ReturnBean getMultipleFiles(@RequestParam("schemafiles") MultipartFile[] schemaFiles,@RequestParam("xmlfile") MultipartFile xmlFile) 
+			throws SAXException, Exception {
 		List<String> schemaFileNameList = new ArrayList<String>(schemaFiles.length);
 		List<String> schemaFileContentList = new ArrayList<String>(schemaFiles.length);
-		try {
+		
 		for(int i=0;i<schemaFiles.length;i++) {
 			MultipartFile file = schemaFiles[i];
-			schemaFileNameList.add(file.getOriginalFilename());			
+			schemaFileNameList.add(core.checkForSlash(file.getOriginalFilename()));			
 				schemaFileContentList.add(new String(file.getBytes(),"UTF-8"));
 		}
 			return service.startValidationProcess(schemaFileNameList, schemaFileContentList,schemaFiles,xmlFile);
-		} catch (IOException | SAXException e) {
-			return new ReturnBean("Error",e.getMessage());
-		}
+	
 	}
 	
 	@PostMapping(value="/validateschemas",consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ReturnBean validateSchemas(@RequestParam("schemafiles") MultipartFile[] schemaFiles) {
+	public ReturnBean validateSchemas(@RequestParam("schemafiles") MultipartFile[] schemaFiles) 
+			throws UnsupportedEncodingException, IOException, InterruptedException, ExecutionException {
 		List<String> schemaFileNameList = new ArrayList<String>(schemaFiles.length);
 		List<String> schemaFileContentList = new ArrayList<String>(schemaFiles.length);
-		try {
+		
 		for(int i=0;i<schemaFiles.length;i++) {
 			MultipartFile file = schemaFiles[i];
-			schemaFileNameList.add(file.getOriginalFilename());			
+			schemaFileNameList.add(core.checkForSlash(file.getOriginalFilename()));			
 				schemaFileContentList.add(new String(file.getBytes(),"UTF-8"));
 		} 
 			return service.validateUploadedSchemas(schemaFileNameList, schemaFileContentList);
-		}catch(IOException e) {
-			 return new ReturnBean("Error",e.getMessage());
-		}
+	
 	 }
 	
 	@PostMapping("/validateWithUrl")
-	public ReturnBean validateWithUrl(@RequestPart("schemaUrl") String schemaUrl,@RequestPart("xmlfile") MultipartFile xmlFile) {
-		try {
+	public ReturnBean validateWithUrl(@RequestPart("schemaUrl") String schemaUrl,@RequestPart("xmlfile") MultipartFile xmlFile) 
+			throws MalformedURLException, SAXException, Exception {
+		
 			return service.validateWithURL(schemaUrl, xmlFile);
-		} catch (IOException | SAXException e) {
-			return new ReturnBean("Error",e.getMessage());
-		}
+		
 	}
 }
